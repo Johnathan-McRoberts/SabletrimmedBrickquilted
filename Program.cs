36b52bb84c42315b8bc4c9e3fcea84ffcf572d02
Johnathan-McRoberts <johnathan.mcroberts@chambers.com>
2026-05-29 11:34:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.OpenApi.Models;
+
+namespace SabletrimmedBrickquilted
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                // The UI title/version label; not the OpenAPI version
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            WebApplication app = builder.Build();
+
+            app.UseSwagger(); // Generates /swagger/v1/swagger.json
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.MapControllers();
+            app.Run();
+
+            //var builder = WebApplication.CreateBuilder(args);
+
+            //// Add services to the container.
+
+            //builder.Services.AddControllers();
+
+            //var app = builder.Build();
+
+            //// Configure the HTTP request pipeline.
+
+            //app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+            //app.MapControllers();
+
+            //app.Run();
+        }
+    }
+}
