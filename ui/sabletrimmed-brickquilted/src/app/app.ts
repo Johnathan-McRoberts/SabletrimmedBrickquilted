f5fff02052cb4b106da0b0af32e522bc62186904
Johnathan-McRoberts <johnathan.mcroberts@chambers.com>
2026-06-01 19:14:18 +0100

    Add material

--- ui/sabletrimmed-brickquilted/src/app/app.ts
+++ ui/sabletrimmed-brickquilted/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSlideToggle],
+  imports: [RouterOutlet, MatSlideToggle, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
