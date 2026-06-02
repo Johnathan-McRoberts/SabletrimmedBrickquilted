13afea0f7f4716cbd5a552968a7b88e3bf397882
Johnathan-McRoberts <johnathan.mcroberts@chambers.com>
2026-06-02 18:22:39 +0100

    Begin creating services

--- ui/sabletrimmed-brickquilted/src/app/app.routes.ts
+++ ui/sabletrimmed-brickquilted/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
+
+
+import { ChartsModule } from './charts/charts-module';
+import { DataFormsModule } from './forms/data-forms-module';
+import { ImportExportModule } from './import-export/import-export-module';
+import { TablesModule } from './tables/tables-module';
+import { UserLoginModule } from './user-login/user-login-module';
+import { SharedModule } from './shared/shared-module';
+
+
+import { loggedInGuard } from './logged-in-guard';
+
 export const routes: Routes = [];
---
+++ ui/sabletrimmed-brickquilted/src/app/shared/models/user-login-request.ts
@@ -0,0 +1,6 @@
+export interface UserLoginRequest {
+
+  name: string;
+  password: string;
+}
+
